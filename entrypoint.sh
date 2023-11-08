1d6cc49f4f86717c2fe6eee3212bfe54aec581c7
Andrei Rybnov <rybnov@dws.sh>
2023-11-08 19:23:37 +0400

    Add 0.1 version of action

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -l
+
+./deploy_script gpu create --access_key $1 --secret_key $2 --ssh_key $3 --name $4 --region $5 --count $6
+
+export RESULT=$(cat result)
+
+echo $RESULT >> $GITHUB_OUTPUT
+
