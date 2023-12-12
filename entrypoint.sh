4555bf407c1b1a948115a6c5ebb138e6409165d2
Andrei Rybnov <rybnov@dws.sh>
2023-12-12 17:29:35 +0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-/usr/bin/deploy_script gpu create --access_key $1 --secret_key $2 --ssh_key $3 --name $4 --region $5 --image $6 --count $7
+/usr/bin/deploy_script gpu create --access_key $1 --secret_key $2 --ssh_key "$3" --name "$4" --region "$5" --image "$6" --count $7
 
 export RESULT=$(cat result)
 
